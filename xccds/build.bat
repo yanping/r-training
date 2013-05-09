75f390782ad6fb76e0a6392b9c6750005c2e8366
yanping <yanping.chen@cos.name>
2013-05-09 17:32:44 +0800

    update

--- xccds/build.bat
+++ xccds/build.bat
@@ -1,2 +1,3 @@
 pandoc -H ../knitr_bootstrap.html intro2r_1.md -o intro2r_1.html
 pandoc -H ../knitr_bootstrap.html intro2r_2.md -o intro2r_2.html
+pandoc -H ../knitr_bootstrap.html intro2r_3.md -o intro2r_3.html
